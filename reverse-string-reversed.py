4c53e23d7c364c3223b24672a8cf418dbdebf9e4
davidhayden <davidhayden@users.noreply.github.com>
2017-12-17 20:56:35 -0700

    Adding Reverse String using reversed.

---
+++ reverse-string-reversed.py
@@ -0,0 +1,24 @@
+"""
+Write a function that takes in a string and
+returns the reversed version of the string.
+"""
+
+def reverse_string(a_string):
+    """
+    Returns reversed version of string.
+
+    >>> reverse_string('abcde')
+    'edcba'
+    >>> reverse_string('1')
+    '1'
+    >>> reverse_string('')
+    ''
+    >>> reverse_string('madam')
+    'madam'
+    """
+    return ''.join(reversed(a_string))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
